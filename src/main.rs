56d0951ebcb7e6e07a5876c6123507618254ded8
Brian Ho <bho@Brians-MacBook-Pro.local>
2015-12-24 15:35:46 -0800

    Cleaned up the example code and added comments.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,40 @@
+// Work in progress of a 2D game using glutin for context creation/input
+// handling and gl-rs for OpenGL bindings. The game will be a simple top down
+// action-RPG created for educational purposes to assess the viability of Rust
+// as a video game development language.
+//
+// Brian Ho
+// brian@brkho.com
+// December 2015
+
+
 extern crate glutin;
 
-use glutin::Window;
+use glutin::{Event, Window};
+use std::process::exit;
+use std::thread::sleep;
+use std::time::Duration;
 
+// Main loop for the game.
 fn main() {
+    // Create the window. Should be using a builder here, but whatever.
     let window = Window::new().unwrap();
+    unsafe { window.make_current().unwrap() };
 
     loop {
+        // poll_events returns an iterator for Event which we match against.
         for event in window.poll_events() {
             match event {
-                // process events here
+                Event::Closed => exit(0),
                 _ => ()
             }
         }
 
-        // draw everything here
-        let _ = window.swap_buffers();
-        let ten_millis = std::time::Duration::from_millis(10);
-        std::thread::sleep(ten_millis);
+        // We can update and draw here after we handle events and swap buffers.
+        window.swap_buffers().unwrap();
+
+        // Sleep one second in between calls.
+        let one_second = Duration::from_millis(1000);
+        sleep(one_second);
     }
 }
